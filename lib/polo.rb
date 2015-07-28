cebb0fbab25f358f910a9704fcdd97c8e09c8e49
Netto Farah <nettofarah@gmail.com>
2015-07-28 13:53:58 -0700

    basic export test case

--- lib/polo.rb
+++ lib/polo.rb
@@ -2,4 +2,37 @@ require "polo/version"
 require "polo/sql_translator"
 
 module Polo
+
+  class Explorer
+    def initialize(base_class, id, dependency_tree={})
+      @base_class = base_class
+      @id = id
+      @dependency_tree = dependency_tree
+    end
+
+    def run
+      selects = []
+      queries_collector = lambda do |name, start, finish, id, payload|
+
+        selects << {
+          class_name: payload[:name].gsub(' Load', '').constantize,
+          sql: payload[:sql]
+        }
+      end
+
+      ActiveSupport::Notifications.subscribed(queries_collector, 'sql.active_record') do
+        @base_class.includes(@dependency_tree).where(id: @id).to_a
+      end
+
+      active_record_instances = selects.flat_map do |select|
+        select[:class_name].find_by_sql(select[:sql]).to_a
+      end
+
+      SqlTranslator.new(active_record_instances).to_sql.uniq
+    end
+  end
+
+  def self.explorer(base_class, id, dependencies)
+    Explorer.new(base_class, id, dependencies)
+  end
 end
--- spec/polo_spec.rb
+++ spec/polo_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
 
 describe Polo do
-  it 'has a version number' do
-    expect(Polo::VERSION).not_to be nil
+
+  before(:all) do
+    TestData.create_netto
+  end
+
+  it 'works with dependencies' do
+    exp = Polo.explorer(AR::Chef, 1, [:recipes])
+
+    inserts = [
+      #"INSERT INTO `chefs` (`id`, `name`) VALUES (1, 'Netto')",
+      "INSERT INTO `recipes` (`id`, `title`, `num_steps`, `chef_id`) VALUES (1, 'Turkey Sandwich', NULL, 1)",
+      "INSERT INTO `recipes` (`id`, `title`, `num_steps`, `chef_id`) VALUES (2, 'Cheese Burger', NULL, 1)"
+    ]
+
+    expect(exp.run).to eq(inserts)
+  end
+
+  it 'works with nested dependencies' do
+    exp = Polo.explorer(AR::Chef, 1, :recipes => :ingredients)
+
+    inserts = [
+      #"INSERT INTO `chefs` (`id`, `name`) VALUES (1, 'Netto')",
+      "INSERT INTO `recipes` (`id`, `title`, `num_steps`, `chef_id`) VALUES (1, 'Turkey Sandwich', NULL, 1)",
+      "INSERT INTO `recipes` (`id`, `title`, `num_steps`, `chef_id`) VALUES (2, 'Cheese Burger', NULL, 1)",
+      "INSERT INTO `recipes_ingredients` (`id`, `recipe_id`, `ingredient_id`) VALUES (1, 1, 1)",
+      "INSERT INTO `recipes_ingredients` (`id`, `recipe_id`, `ingredient_id`) VALUES (2, 1, 2)",
+      "INSERT INTO `recipes_ingredients` (`id`, `recipe_id`, `ingredient_id`) VALUES (3, 2, 3)",
+      "INSERT INTO `recipes_ingredients` (`id`, `recipe_id`, `ingredient_id`) VALUES (4, 2, 4)",
+      "INSERT INTO `ingredients` (`id`, `name`, `quantity`) VALUES (1, 'Turkey', 'a lot')",
+      "INSERT INTO `ingredients` (`id`, `name`, `quantity`) VALUES (2, 'Cheese', '1 slice')",
+      "INSERT INTO `ingredients` (`id`, `name`, `quantity`) VALUES (3, 'Patty', '1')",
+      "INSERT INTO `ingredients` (`id`, `name`, `quantity`) VALUES (4, 'Cheese', '2 slices')"
+    ]
+
+    expect(exp.run).to eq(inserts)
   end
 end
