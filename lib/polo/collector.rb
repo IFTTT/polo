bc8565b6803b5e5a48d013da74318834a35d3ce7
Netto Farah <nettofarah@gmail.com>
2015-07-28 16:56:26 -0700

    move basic docs to polo.rb

--- lib/polo/collector.rb
+++ lib/polo/collector.rb
@@ -1,36 +1,6 @@
 module Polo
   class Collector
 
-    # Public: Creates a new Polo::Collector
-    #
-    # base_class - An ActiveRecord::Base class for the seed record.
-    # id - An ID used to find the desired seed record.
-    #
-    # dependency_tree - An ActiveRecord::Associations::Preloader compliant that
-    # will define the path Polo will traverse.
-    #
-    # (from ActiveRecord::Associations::Preloader docs)
-    # It may be:
-    # - a Symbol or a String which specifies a single association name. For
-    #   example, specifying +:books+ allows this method to preload all books
-    #   for an Author.
-    # - an Array which specifies multiple association names. This array
-    #   is processed recursively. For example, specifying <tt>[:avatar, :books]</tt>
-    #   allows this method to preload an author's avatar as well as all of his
-    #   books.
-    # - a Hash which specifies multiple association names, as well as
-    #   association names for the to-be-preloaded association objects. For
-    #   example, specifying <tt>{ author: :avatar }</tt> will preload a
-    #   book's author, as well as that author's avatar.
-    #
-    # +:associations+ has the same format as the +:include+ option for
-    # <tt>ActiveRecord::Base.find</tt>. So +associations+ could look like this:
-    #
-    #   :books
-    #   [ :books, :author ]
-    #   { author: :avatar }
-    #   [ :books, { author: :avatar } ]
-    #
     def initialize(base_class, id, dependency_tree={})
       @base_class = base_class
       @id = id
