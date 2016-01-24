561ef5479e0ac260fc427c8e8e7d2bdc17f55d1a
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-01-24 15:34:08 -0800

    flesh out test suite

--- lib/polo/csv_translator.rb
+++ lib/polo/csv_translator.rb
@@ -5,15 +5,16 @@ require 'csv'
 module Polo
   class CSVTranslator
     def initialize(table, csv_file, configuration=Configuration.new)
-      # (.csv)$/.match(csv_file)? @csv = csv_file :
+      raise ArgumentError, "file name must end in .csv" unless /(.csv)$/.match(csv_file)
+      @csv = csv_file
       @table = table
       @configuration = configuration
     end
 
     def to_csv
-        CSV.open(@csv, "w", write_hearders: true, headers: @table.column_names) do |csv|
+        CSV.open(@csv, "w", write_headers: true, headers: @table.column_names) do |csv|
           @table.all.each do |row|
-            csv << row.values
+            csv << row.attributes.values
           end
         end
     end
--- spec/csv_translator_spec.rb
+++ spec/csv_translator_spec.rb
@@ -1,17 +1,47 @@
 require 'spec_helper'
+require 'fileutils' #to remove files when done
 
 describe  Polo::CSVTranslator do
-
-  let(:netto) do
-    AR::Chef.where(name: 'Netto').first
-  end
 
   before(:all) do
     TestData.create_netto
   end
 
-  it 'prevents instantiation if not csv file' do
-    expect(Polo::CSVTranslator.new(AR::Chef, "fail")).to raise_error
+  it 'instantiates CSVTranslator' do
+    expect{Polo::CSVTranslator.new(AR::Chef, "test.csv")}.not_to raise_error
   end
 
+  it 'prevents instantiation if not csv file' do
+    expect{Polo::CSVTranslator.new(AR::Chef, "notcsv.txt")}.to raise_error(ArgumentError, "file name must end in .csv")
+  end
+
+  it 'prevents instantiation if table not in database' do
+    expect{Polo::CSVTranslator.new(AR::NotATable, "test.csv")}.to raise_error(NameError)
+  end
+
+  describe 'to_csv' do
+    let(:translator) {Polo::CSVTranslator.new(AR::Chef, "test.csv")}
+    let(:translator2) {Polo::CSVTranslator.new(AR::Recipe, "test2.csv")}
+
+    it 'creates a new file with csv name' do
+      translator.to_csv
+      expect(File.exist?("test.csv")).to be_truthy
+    end
+
+    it 'adds headers to csv' do
+      expect(CSV.readlines("test.csv")[0]).to match_array ["id", "name", "email"]
+    end
+
+    it 'adds all rows to csv file' do
+      translator2.to_csv
+      expect(CSV.readlines("test2.csv").size).to equal(AR::Recipe.count+1)
+    end
+
+  end
+
+  after(:all) do
+    FileUtils.rm("test.csv")
+    FileUtils.rm("test2.csv")
+  end
 end
+
