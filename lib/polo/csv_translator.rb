14698bfab5eb05f4c94bda2b740526d05c68b2d6
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-01-24 15:38:32 -0800

    add notes

--- lib/polo/csv_translator.rb
+++ lib/polo/csv_translator.rb
@@ -1,19 +1,22 @@
-# require 'active_record'
-# require 'polo/configuration'
 require 'csv'
 
 module Polo
   class CSVTranslator
-    def initialize(table, csv_file, configuration=Configuration.new)
+
+    # Tool to translate table to csv file
+    # Future updates: translate CSV to table
+
+    def initialize(model, csv_file, configuration=Configuration.new)
       raise ArgumentError, "file name must end in .csv" unless /(.csv)$/.match(csv_file)
       @csv = csv_file
-      @table = table
+      @model = model
       @configuration = configuration
     end
 
     def to_csv
-        CSV.open(@csv, "w", write_headers: true, headers: @table.column_names) do |csv|
-          @table.all.each do |row|
+      #
+        CSV.open(@csv, "w", write_headers: true, headers: @model.column_names) do |csv|
+          @model.all.each do |row|
             csv << row.attributes.values
           end
         end
