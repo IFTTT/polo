d3c22e7d7870c8f8ac23b017e300cb44d5819242
Jonathan del Strother <jdelStrother@gmail.com>
2016-08-20 09:00:45 +0100

    Escape columns in DUPLICATE KEY statements (#42)

--- lib/polo/adapters/mysql.rb
+++ lib/polo/adapters/mysql.rb
@@ -5,7 +5,7 @@ module Polo
         insert_and_record = inserts.zip(records)
         insert_and_record.map do |insert, record|
           values_syntax = record.attributes.keys.map do |key|
-            "#{key} = VALUES(#{key})"
+            "`#{key}` = VALUES(`#{key}`)"
           end
 
           on_dup_syntax = "ON DUPLICATE KEY UPDATE #{values_syntax.join(', ')}"
